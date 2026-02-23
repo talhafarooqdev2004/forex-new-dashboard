d253d3544e9899ad044a0b4a4e4d98d85250bec1
talhafarooqdev2004 <talhafarooq.dev.2004@gmail.com>
2026-02-23 22:39:24 +0500

    remove temp folder

--- Retail Sentiment Chart/src/app/App.tsx
+++
@@ -1,9 +0,0 @@
-import { RetailSentimentChart } from "./components/RetailSentimentChart";
-
-export default function App() {
-  return (
-    <div className="size-full flex items-center justify-center bg-[#121417] p-6">
-      <RetailSentimentChart />
-    </div>
-  );
-}
